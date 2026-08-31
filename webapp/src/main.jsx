4c44af3cea19c7202875bdeffcdc1a90fe7c28cb
amirramzan786 <U1165153@unimail.hud.ac.uk>
2026-08-31 10:48:16 +0100

    Load account settings styles

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import AuthGate from './AuthGate'
 import './styles.css'
+import './settings.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
