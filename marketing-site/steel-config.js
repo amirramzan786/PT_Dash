c8fdf249190ad6fecdda71ad60b387c18497377a
Amir <amir@Amirs-MacBook-Air.local>
2026-09-05 18:13:33 +0100

    chore: configure public Steel Turnstile site key

--- marketing-site/steel-config.js
+++ marketing-site/steel-config.js
@@ -3,6 +3,6 @@
 window.STEEL_CONFIG = {
   supabaseUrl: 'https://devpjwpirhhctrwizzab.supabase.co',
   supabasePublishableKey: 'sb_publishable_OMOuou7cTDMsEeBJtxKjtA_ky3a_SmL',
-  turnstileSiteKey: '',
+  turnstileSiteKey: '0x4AAAAAAEpXXz4ouCGqwReO',
   appUrl: 'https://pt-dash.pages.dev',
 }
